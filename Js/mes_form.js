0942fae5b524a07778ee503d2986169b18db50c4
Prashan Rasiah <Prashan.Rasiah@sl.iec.local>
2022-11-22 16:32:12 +0530

    Background infromation updated

--- Js/mes_form.js
+++ Js/mes_form.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    $(".if_check").hide();
+
     $('input[type="radio"]').click(function () {
         if ($(this).is(':checked')) {
             var parent = $(this).parents(".input_group,.input_group_inline");
@@ -6,7 +8,6 @@ $(document).ready(function () {
             if ($(this).val() == 1) {
                 parent.find(".if_yes").css("display", "block");
                 parent.find(".if_no").css("display", "none");
-                parent.find(".if_check").css("display", "none");
             } else if ($(this).val() == 0) {
                 parent.find(".if_yes").css("display", "none");
                 parent.find(".if_no").css("display", "block");
@@ -14,13 +15,19 @@ $(document).ready(function () {
             parent.find(".explainArea").css("display", "block");
         }
     });
+    
     $('input[class="radioCheck"]').on('change', function() {
-      var parent = $(this).parents(".input_group,.input_group_inline");
-      $('input[class="radioCheck"]').not(this).prop('checked', false);
-      parent.find(".if_yes").css("display", "block");
-      parent.find(".if_check").css("display", "block");
-      parent.find(".if_no").css("display", "none");
-   });
+        
+       $(".if_check").hide();
+        var parent = $(this).parents(".checkbox_input_group,.checkbox_input_group_inline");
+        if ($(this).is(":checked")) { 
+            $('input[class="radioCheck"]').not(this).prop('checked', false);
+            
+            parent.find(".if_check").css("display", "block"); 
+        }else{
+            $(".if_check").hide();
+        }
+    });
    
 });
 
